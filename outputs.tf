75ec85dc735ac491aa7b041faa3eca7e17961bfb
skbasha-devops <khaju452@gmail.com>
2025-07-27 21:15:38 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "files" {
-value = [
-local_file.file1.filename,
-local_file.file2.filename
-}
+  value = [
+    local_file.file1.filename,
+    local_file.file2.filename
+  ]
 }
 
 output "directories" {
-value = [
-  local_file.file1.filename,
-  local_file.file2.filename
-}  # This should be ]
+  value = [
+    dirname(local_file.dir1_placeholder.filename),
+    dirname(local_file.dir2_placeholder.filename)
+  ]
 }
